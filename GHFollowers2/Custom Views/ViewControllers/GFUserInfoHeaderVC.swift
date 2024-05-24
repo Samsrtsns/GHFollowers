a40acc0e291f2555f55d7cc75efc3d80f8b0680b
Abdussamed Katı <94466259+Samsrtsns@users.noreply.github.com>
2024-05-24 17:02:58 +0300

    add

---
+++ GHFollowers2/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,17 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GHFollowers2
+//
+//  Created by Samet KATI on 24.05.2024.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
