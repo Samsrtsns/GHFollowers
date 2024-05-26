98c0f694799fac4f99cc08020f593560df86db63
Abdussamed Katı <94466259+Samsrtsns@users.noreply.github.com>
2024-05-27 02:19:33 +0300

    Info Screen Header done

--- GHFollowers2/Custom Views/Labels/GFSecondaryTitleLabel.swift
+++ GHFollowers2/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -7,14 +7,43 @@
 
 import UIKit
 
+// GFSecondaryTitleLabel adlı özel bir UILabel sınıfı oluşturuyoruz.
 class GFSecondaryTitleLabel: UILabel {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // Standart init metodunu override ediyoruz. Bu metod, sınıfın programatik olarak başlatılması durumunda çağrılır.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    // Interface Builder (Storyboard veya XIB) tarafından kullanıldığında çağrılması gereken init metodu.
+    // Bu projede kullanılmadığı için fatalError ile uygulama çökertecek şekilde ayarlandı.
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+    // Kullanıcı tarafından belirtilen font büyüklüğü ile sınıfı başlatan özel init metodu.
+    init(fontSize : CGFloat){
+        // UILabel'in init(frame:) metodunu çağırıyoruz ve çerçeve olarak .zero (yani boş bir çerçeve) belirtiyoruz.
+        super.init(frame: .zero)
+        // Fontu, belirli bir büyüklük ve ağırlık (medium) ile ayarlıyoruz.
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        // Ek yapılandırmaları yapmak için configure() metodunu çağırıyoruz.
+        configure()
+    }
+    
+    // Label'ın çeşitli özelliklerini ayarlayan özel yapılandırma metodu.
+    private func configure(){
+        // Label'ın metin rengini, iOS'un ikincil etiket renginde ayarlıyoruz (genellikle gri tonlarında).
+        textColor = .secondaryLabel
+        // Metin boyutunun etiket genişliğine sığacak şekilde otomatik olarak ayarlanmasını sağlıyoruz.
+        adjustsFontSizeToFitWidth = true
+        // Metin boyutunun minimum ölçekleme faktörünü belirliyoruz (burada %90).
+        minimumScaleFactor = 0.90
+        // Metin çok uzun olduğunda sonunu keserek '...' eklenmesini sağlıyoruz.
+        lineBreakMode = .byTruncatingTail
+        // Otomatik yerleşim kısıtlamalarının kullanılacağını belirtiyoruz.
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 
 }
+
---
+++ GHFollowers2/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers2
+//
+//  Created by Samet KATI on 27.05.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
