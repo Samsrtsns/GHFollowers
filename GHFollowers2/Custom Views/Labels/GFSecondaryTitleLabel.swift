6f92a5f9dded620111226447ee05a56fc132d5bc
Abdussamed Katı <94466259+Samsrtsns@users.noreply.github.com>
2024-05-26 22:42:03 +0300

    add secondary title label

---
+++ GHFollowers2/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,20 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GHFollowers2
+//
+//  Created by Samet KATI on 26.05.2024.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
