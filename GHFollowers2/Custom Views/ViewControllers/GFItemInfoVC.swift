7609e6a3f502b304ae9c6b3259e629c061020a3b
Abdussamed Katı <94466259+Samsrtsns@users.noreply.github.com>
2024-05-30 15:06:24 +0300

    Item Info Detailed Done

---
+++ GHFollowers2/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -0,0 +1,16 @@
+//
+//  GFItemInfoVC.swift
+//  GHFollowers2
+//
+//  Created by Samet KATI on 30.05.2024.
+//
+
+import UIKit
+
+class GFItemInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
